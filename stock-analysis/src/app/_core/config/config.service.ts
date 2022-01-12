06f49892d1145f821c57727d77f282828bfab377
Noel Huibers <noel@huibers.io>
2022-01-12 14:51:08 +0100

    Merge

--- stock-analysis/src/app/_core/config/config.service.ts
+++ stock-analysis/src/app/_core/config/config.service.ts
@@ -5,11 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
 
-<<<<<<< HEAD
-  BASE_URL = "http://127.0.0.1:5000/"
-=======
     BASE_URL = "https://craws.uber.space/"
->>>>>>> 5b60650cb59dc0ccef3def1772728dcaa69955b8
 
   constructor() { }
 }
